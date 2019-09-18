cb698a6a32be007879b62fc66699ad363ba1fcf9
server <admin@jiruiwangluo.com>
2019-09-18 12:28:15 +0800

    add debug

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+define('DEBUG', 0);
+if( DEBUG ) {
+    ini_set('display_errors' , 1);
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors' , 0);
+    error_reporting(E_ALL & ~E_NOTICE);
+}
 define('APP_DIR', realpath('./'));
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 define('INCL_DIR', APP_DIR.DS.'protected'.DS.'include');
